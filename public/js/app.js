9e0add3ce812a9a8028b903658deb8528effc91f
bonny_J <2309505407@qq.com>
2020-04-29 00:25:36 +0800

    静态文件浏览器缓存问题

---
+++ public/js/app.js
@@ -0,0 +1,124 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'C:\\Users\\J\\code\\laravel\\node_modules\\schema-utils\\src\\index.js'. Please verify that the package.json has a valid \"main\" entry\n    at tryPackage (internal/modules/cjs/loader.js:297:19)\n    at Function.Module._findPath (internal/modules/cjs/loader.js:528:18)\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:784:27)\n    at Function.Module._load (internal/modules/cjs/loader.js:690:27)\n    at Module.require (internal/modules/cjs/loader.js:852:19)\n    at require (C:\\Users\\J\\code\\laravel\\node_modules\\v8-compile-cache\\v8-compile-cache.js:161:20)\n    at Object.<anonymous> (C:\\Users\\J\\code\\laravel\\node_modules\\babel-loader\\lib\\index.js:43:25)\n    at Module._compile (C:\\Users\\J\\code\\laravel\\node_modules\\v8-compile-cache\\v8-compile-cache.js:192:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:995:10)\n    at Module.load (internal/modules/cjs/loader.js:815:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:727:14)\n    at Module.require (internal/modules/cjs/loader.js:852:19)\n    at require (C:\\Users\\J\\code\\laravel\\node_modules\\v8-compile-cache\\v8-compile-cache.js:161:20)\n    at loadLoader (C:\\Users\\J\\code\\laravel\\node_modules\\loader-runner\\lib\\loadLoader.js:18:17)\n    at iteratePitchingLoaders (C:\\Users\\J\\code\\laravel\\node_modules\\loader-runner\\lib\\LoaderRunner.js:169:2)\n    at runLoaders (C:\\Users\\J\\code\\laravel\\node_modules\\loader-runner\\lib\\LoaderRunner.js:365:2)\n    at NormalModule.doBuild (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\NormalModule.js:295:3)\n    at NormalModule.build (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\NormalModule.js:446:15)\n    at Compilation.buildModule (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:739:10)\n    at C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:981:14\n    at NormalModuleFactory.create (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\NormalModuleFactory.js:376:26)\n    at C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:897:14\n    at Semaphore.acquire (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\util\\Semaphore.js:29:4)\n    at asyncLib.forEach.err.stack (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:895:15)\n    at arrayEach (C:\\Users\\J\\code\\laravel\\node_modules\\neo-async\\async.js:2405:9)\n    at Object.each (C:\\Users\\J\\code\\laravel\\node_modules\\neo-async\\async.js:2846:9)\n    at Compilation.addModuleDependencies (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:873:12)\n    at Compilation.processModuleDependencies (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:843:8)\n    at afterBuild (C:\\Users\\J\\code\\laravel\\node_modules\\webpack\\lib\\Compilation.js:1095:13)\n    at processTicksAndRejections (internal/process/task_queues.js:75:11)");
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! C:\Users\J\code\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\J\code\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+
+
+/***/ })
+
+/******/ });
